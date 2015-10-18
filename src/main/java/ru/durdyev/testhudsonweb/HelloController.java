390060d3369c57ea14e6cae7ed20a6fa11b82566
Ilya Durdyev <ilya.durdyev@lifeit.ru>
2015-10-18 13:04:23 +0300

    stamping version

--- src/main/java/ru/durdyev/testhudsonweb/HelloController.java
+++ src/main/java/ru/durdyev/testhudsonweb/HelloController.java
@@ -5,12 +5,16 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ResourceBundle;
+
 @Controller
 @RequestMapping("/")
 public class HelloController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
+		ResourceBundle bundle = ResourceBundle.getBundle("version");
 		model.addAttribute("message", "Hello world!");
+		model.addAttribute("version", bundle.getString("version"));
 		return "hello";
 	}
 }
